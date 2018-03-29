dd8a5842b654e256bb798a5ada3ecb1f1ba5350a
lucas gonçalves <lucasg_f@outlook.com>
2018-03-29 19:26:10 -0300

    melhorias no projeto

--- src/main/java/com/todoapp/DataConfigurationPostgreSQL.java
+++ src/main/java/com/todoapp/DataConfigurationPostgreSQL.java
@@ -1,5 +1,6 @@
 package com.todoapp;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -12,10 +13,11 @@ import org.springframework.context.annotation.Profile;
 public class DataConfigurationPostgreSQL {
 	@Bean
     public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = "tdhimrjnwqcqzm";
-        String password = "8784c42710b8868f63fb70b0bad3a783d2d769dce1713e4e124bf5280d508a01";
-        String dbUrl = "jdbc:postgresql://postgres://tdhimrjnwqcqzm:8784c42710b8868f63fb70b0bad3a783d2d769dce1713e4e124bf5280d508a01@ec2-54-243-28-109.compute-1.amazonaws.com:5432/d4k3e95c606ooa";
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
@@ -24,4 +26,5 @@ public class DataConfigurationPostgreSQL {
 
         return basicDataSource;
     }
+
 }
